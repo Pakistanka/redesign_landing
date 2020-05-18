5b28df25bf9c6e0e2b23a5cbd93c38b59ef7b502
mac <mac@macs-MBP.fritz.box>
2020-05-18 11:53:13 +0200

    clean code

--- js/animaiton.js
+++ js/animaiton.js
@@ -6,15 +6,14 @@ var w = screen.width;
     let base_open = true; 
     base_opener.onclick = toggleBase = () => {
         if (base_open){
-            gsap.to('.s_panel__base-dynamic-el', {duration: .3, opacity: 0});
-            gsap.to('.s_panel__base-dynamic-el', {delay: .3, visibility: "hidden", });
+            gsap.to('.s_panel__base-dynamic-el', {duration: .3, autoAlpha: 0});
             gsap.to('.s_panel__base_selector', {delay: .3, height: '12px'});
             gsap.to('.s_panel__base_label__show', {duration: .1, rotation: -90});
             base_open = false;
             
         } else if (base_open === false){
             gsap.to('.s_panel__base-dynamic-el',{duration: .3, y: 0});
-            gsap.to('.s_panel__base-dynamic-el',{delay: .3, visibility: "visible", opacity: 1});
+            gsap.to('.s_panel__base-dynamic-el',{delay: .3, autoAlpha: 1});
             gsap.to('.s_panel__base_selector', {height: '154px'});
             gsap.to('.s_panel__base_label__show', {duration: .1, rotation: 0})
             base_open = true;
@@ -26,16 +25,14 @@ var w = screen.width;
     color_open = true; 
     color_opener.onclick = toggleBase = () => {
         if (color_open){
-            gsap.to('.s_panel__color_piker', {duration: .3, opacity: 0});
-            gsap.to('.s_panel__color_piker', {delay: .3, visibility: "hidden", });
+            gsap.to('.s_panel__color_piker', {duration: .3, autoAlpha: 0});
             gsap.to('.s_panel__color_selector', {delay: .3, height: '12px'});
-            gsap.to('.s_panel__color_label__show', {duration: .1, rotation: -90});
+            gsap.to('.s_panel__color_label__show', {duration: .1, rotation: "-90"});
             color_open = false;
             
         } else if (color_open === false){
-            gsap.to('.s_panel__color_piker',{duration: .3, y: 0});
-            gsap.to('.s_panel__color_piker',{delay: .3, visibility: "visible", opacity: 1});
-            gsap.to('.s_panel__color_selector', {height: '84px'});
+            gsap.to('.s_panel__color_selector', {duration: .3, height: '84px'});
+            gsap.to('.s_panel__color_piker',{delay: .3, duration: .3,  autoAlpha: 1});
             gsap.to('.s_panel__color_label__show', {duration: .1, rotation: 0})
             color_open = true;
         }
@@ -141,152 +138,6 @@ if (w >= 768){
     }
 }
 
-
-
-// if (w > 1199){
-//     let ssettings = document.querySelector(".settings");
-//     let ss_panel_close_btn = document.querySelector(".s_panel_close_btn");
-
-//     ssettings.onclick = showPanel = () => {
-//         gsap.to(".s_panel_desktop", {  visibility: 'visible'});
-//         gsap.to(".s_panel_desktop", {  duration: .5, autoAlpha: 1});
-//         gsap.to('.s_panel_close_btn', { delay: .5, autoAlpha: 1 });
-//         gsap.to('.settings', { duration: .5, ease: "power4.out", autoAlpha: 0 })
-//     }
-    
-//     ss_panel_close_btn.onclick = hidePanel = () => {
-//         gsap.to(".s_panel_desktop", { duration: .5, autoAlpha: 0});
-//         gsap.to('.s_panel_close_btn', { autoAlpha: 0},"-=1");
-//         gsap.to('.settings', { duration: .5, ease: "power4.out", autoAlpha: 1 });
-//     }
-
-
-
-
-//     let base_opener = document.querySelector(".open_base");
-//     let base_open = true; 
-//     base_opener.onclick = toggleBase = () => {
-//         console.log('base clicked!')
-//         if (base_open){
-//             gsap.to('.s_panel_desktop__base-dynamic-el', {duration: .3, opacity: 0});
-//             gsap.to('.s_panel_desktop__base-dynamic-el', {delay: .3, visibility: "hidden", });
-//             gsap.to('.s_panel_desktop__base_selector', {delay: .3, height: '12px'});
-//             gsap.to('.s_panel_desktop__base_label__show', {duration: .1, rotation: -90});
-//             base_open = false;
-            
-//         } else if (base_open === false){
-//             gsap.to('.s_panel_desktop__base-dynamic-el',{duration: .3, y: 0});
-//             gsap.to('.s_panel_desktop__base-dynamic-el',{delay: .3, visibility: "visible", opacity: 1});
-//             gsap.to('.s_panel_desktop__base_selector', {height: '100px'});
-//             gsap.to('.s_panel_desktop__base_label__show', {duration: .1, rotation: 0})
-//             base_open = true;
-//         }
-//     }
-
-
-//     let color_opener = document.querySelector(".color_opener");
-//     let color_open = true; 
-//     color_opener.onclick = toggleBase = () => {
-//         if (color_open){
-//             gsap.to('.s_panel_desktop__color_piker', {duration: .3, opacity: 0});
-//             gsap.to('.s_panel_desktop__color_piker', {delay: .3, visibility: "hidden", });
-//             gsap.to('.s_panel_desktop__color_selector', {delay: .3, height: '12px'});
-//             gsap.to('.s_panel_desktop__color_label__show', {duration: .1, rotation: -90});
-//             color_open = false;
-            
-//         } else if (color_open === false){
-//             gsap.to('.s_panel_desktop__color_piker',{duration: .3, y: 0});
-//             gsap.to('.s_panel_desktop__color_piker',{delay: .3, visibility: "visible", opacity: 1});
-//             gsap.to('.s_panel_desktop__color_selector', {height: '84px'});
-//             gsap.to('.s_panel_desktop__color_label__show', {duration: .1, rotation: 0})
-//             color_open = true;
-//         }
-//     }
-
-    
-    
-//     let baseColor = document.querySelector(".move_color");
-//     let basePattern = document.querySelector(".move_pattern");
-    
-//     baseColor.onclick = moveToPattern = () => {
-//         gsap.to('.underColorPattern', {duration: .3, marginLeft: '0'});
-//     }
-    
-//     basePattern.onclick = moveToColor = () => {
-//         gsap.to('.underColorPattern', {duration: .3, marginLeft: '50%'});
-//     }
-
-    
-
-
-//     let blockAbout = document.querySelector(".show_about"); 
-//     let blockFeedback = document.querySelector(".show_info");
-    
-//     let highLightAbout = false;
-//     let highLightFeedback = false;
-    
-    
-//     blockAbout.onclick = toggleAbout = () => {
-//         if(highLightAbout===false){
-//             gsap.to('.s_panel__info_about', {duration: .2, background: '#fff'});
-//             gsap.to('.s_panel__info_wrap', {duration: .2, background: 'transparent'});
-//             gsap.to('.s_panel__info--block',{durtation: .2, display: "block"});
-//             gsap.to('.s_panel__info--block',{durtation: .2, autoAlpha: 1});
-    
-//             highLightAbout = true;
-//             highLightFeedback = false;
-//         } else if (highLightAbout){
-//             gsap.to('.s_panel__info_about', {duration: .2, background: 'transparent'});
-//             gsap.to('.s_panel__info--block',{durtation: .2, display: "none"});
-//             gsap.to('.s_panel__info--block',{durtation: .3, autoAlpha: 0});
-//             // remove block below
-//             highLightAbout = false;
-//         }
-//     }
-    
-//     blockFeedback.onclick = toggleFeedback = () => {
-//         if (highLightFeedback === false){
-//             gsap.to('.s_panel__info_about', {duration: .2, background: 'transparent'});
-//             gsap.to('.s_panel__info_wrap', {duration: .2, background: '#fff'});
-//             gsap.to('.s_panel__info--block',{durtation: .2, display: "block"});
-//             gsap.to('.s_panel__info--block',{durtation: .3, autoAlpha: 1});
-        
-//             highLightAbout = false;
-//             highLightFeedback = true;
-    
-//         } else if (highLightFeedback){
-//             gsap.to('.s_panel__info_wrap', {duration: .2, background: 'transparent'});
-//             gsap.to('.s_panel__info--block',{durtation: .2, display: "none"});
-//             gsap.to('.s_panel__info--block',{durtation: .3, autoAlpha: 0});
-//             // remove block below
-//             highLightFeedback = false;
-//         }
-//     }
-// }
-
-
-
-// let spanelMen = document.querySelector(".mob_sp_m")
-// let spanelWomen = document.querySelector(".spanel_women")
-// let spanelKids = document.querySelector(".spanel_kids")
-
-// gsap.to('.mob_sp_m',{backgroundColor: '#000', color: "#fff"});
-// spanelMen.onclick = onMen = () => {
-//     gsap.to('.mob_sp_m',{backgroundColor: '#000', color: "#fff"});
-//     gsap.to('.spanel_women',{backgroundColor: '#fff', color: "#000"});
-//     gsap.to('.spanel_kids',{backgroundColor: '#fff', color: "#000"});
-// }
-
-// spanelWomen.onclick = onWomen = () => {
-//     gsap.to('.mob_sp_m',{backgroundColor: '#fff', color: "#000"});
-//     gsap.to('.spanel_women',{backgroundColor: '#000', color: "#fff"});
-//     gsap.to('.spanel_kids',{backgroundColor: '#fff', color: "#000"});
-// }
-// spanelKids.onclick = onKids = () => {
-//     gsap.to('.mob_sp_m',{backgroundColor: '#fff', color: "#000"});
-//     gsap.to('.spanel_women',{backgroundColor: '#fff', color: "#000"});
-//     gsap.to('.spanel_kids',{backgroundColor: '#000', color: "#fff"});
-// }
 
 let mobSpM = document.querySelector('.mob_sp_m');
 let mobSpW = document.querySelector('.mob_sp_w');
